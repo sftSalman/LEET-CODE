d6edf83dc7e48f33fef8dce3e03ee0fc2a60aa2a
salmanFarshi <salmanfarshinsu@gmail.com>
2022-09-02 09:49:06 +0600

    with ds

--- 26_remove duplicate_sorted_array.py
+++ 26_remove duplicate_sorted_array.py
@@ -1,6 +1,11 @@
-arr = [0,0,1,1,1,2,2,3,3,4]
+class Solution:
+    def removeDuplicate(self,nums:list[int])->int:
+        left_pointer = 1
+        for r  in range(1,len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[left_pointer] = nums[r]
+                left_pointer+=1
+        return left_pointer
 
-myArr = list(dict.fromkeys(arr))
-ln = len(myArr)
-print(myArr)
-print(ln)
+ob = Solution()
+print(ob.removeDuplicate([1,1,2]))
