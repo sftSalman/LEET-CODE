44ae28501fa04179fdb42af6c349556982a859b5
salmanFarshi <salmanfarshinsu@gmail.com>
2022-10-02 12:02:39 +0600

    array in python from https://www.guru99.com/python-arrays.html

---
+++ python_array_details_guru.py
@@ -0,0 +1,28 @@
+import array
+balance = array.array('i',[300,200,100,30,22,2,2])
+print(balance[1:3])
+
+# inserting element
+
+balance.insert(3,999)
+
+# modifying array
+
+
+balance[3]=1111
+print(balance[3])
+
+# pop element in the array
+
+balance.pop(3)
+print(balance)
+
+
+# delete the element from array
+
+balance.remove(2)
+print(balance)
+
+# searhing from array
+
+print(balance.index(30))
